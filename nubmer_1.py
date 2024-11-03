ff51bbbb008d3a82290b93f08e1060ebe7f0ac30
Sergey-2005 <94121977+sergey-2005@users.noreply.github.com>
2024-11-04 00:55:30 +0300

    Add files via upload

---
+++ nubmer_1.py
@@ -0,0 +1,15 @@
+# TODO Напишите функцию для поиска индекса товара
+def IPV(items_list, find_item):
+    for i in range(len(items_list)):
+        if (items_list[i] == find_item):
+            return i
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = IPV(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
