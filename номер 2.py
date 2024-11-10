43f5f107e4e1d4e8fc4a61c271bcbd01127de9ee
Sergey-2005 <94121977+sergey-2005@users.noreply.github.com>
2024-11-10 23:44:32 +0300

    Add files via upload

--- номер 2.py
+++ номер 2.py
@@ -1,10 +1,21 @@
-salary = 5000  # Ежемесячная зарплата
-spend = 6000  # Траты за первый месяц
-months = 10  # Количество месяцев, которое планируется протянуть без долгов
-increase = 0.03  # Ежемесячный рост цен
+# TODO импортировать необходимые молули
 
-# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
-money_capital = 0
-for i in range(months):
-    money_capital += (spend * (1 + increase) ** i) - salary
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital))
+import csv
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+
+def task() -> None:
+    ...  # TODO считать содержимое csv файла
+    with open(INPUT_FILENAME) as f:
+    lines = [line for line in csv.DictReader(f)]
+    ...  # TODO Сериализовать в файл с отступами равными 4
+
+
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
