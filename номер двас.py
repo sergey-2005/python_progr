1b6f5cdb6eb6c04e3331048550c7e281a63bd63a
Sergey-2005 <94121977+sergey-2005@users.noreply.github.com>
2024-11-11 01:31:33 +0300

    Update номер двас.py

--- номер двас.py
+++ номер двас.py
@@ -1,15 +1,19 @@
 # TODO импортировать необходимые молули
 
 import csv
+import json
 INPUT_FILENAME = "input.csv"
 OUTPUT_FILENAME = "output.json"
 
 
 def task() -> None:
-    ...  # TODO считать содержимое csv файла
-    with open(INPUT_FILENAME) as f:
-    lines = [line for line in csv.DictReader(f)]
-    ...  # TODO Сериализовать в файл с отступами равными 4
+    # TODO считать содержимое csv файла
+    with open(INPUT_FILENAME) as fcs:
+        lines = [line for line in csv.DictReader(fcs)]
+        with open(OUTPUT_FILENAME, "w") as fjs:
+            json.dump(lines, fjs, indent=4)
+
+        # TODO Сериализовать в файл с отступами равными 4
 
 
 if __name__ == '__main__':
